1a235e25fd44e4fc1642a7caeefcadff9aed45d6
kaykeragoso <kaykucasrb@gmail.com>
2025-08-12 20:24:04 -0300

    Começando a usar o DOM

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+amigos = [];
+function adicionarAmigo() {
+    let nome = document.getElementById("amigo").value.trim();
+    if(nome){
+        amigos.push(nome);
+        console.log(amigos);
+        document.getElementById("amigo").value = "";
+    }else{
+        console.log("Digite um nome antes de adicionar.");
+    }
+}
